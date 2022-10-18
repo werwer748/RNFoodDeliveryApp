60840991c8018bb0a3abbb9a8e5b02ff9253ff04
werwer748 <werwer748@naver.com>
2022-10-19 00:10:59 +0900

    티맵 연동중... 이런거 할 때는 오래걸림

--- android/app/src/main/java/com/hugo/fooddeliveryapp/MainApplication.java
+++ android/app/src/main/java/com/hugo/fooddeliveryapp/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
+import com.hugo.fooddeliveryapp.TMapPackage;
 import com.hugo.fooddeliveryapp.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new TMapPackage());
           return packages;
         }
 
